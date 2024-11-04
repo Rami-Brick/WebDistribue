c5ce17eaf737b46e9c5e144218644beae6d9df16
Med Rami Brick <brickrami1@gmail.com>
2024-11-04 10:17:33 +0100

    setup eureka server

--- src/main/java/esprit/tn/webdistribue/WebDistribueApplication.java
+++ src/main/java/esprit/tn/webdistribue/WebDistribueApplication.java
@@ -2,8 +2,10 @@ package esprit.tn.webdistribue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebDistribueApplication {
 
     public static void main(String[] args) {
