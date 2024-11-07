0a38ea2f5a5db086b7bb943cc98648879296e43b
Med Rami Brick <brickrami1@gmail.com>
2024-11-07 15:38:04 +0100

    Eureka server working

--- eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package esprit.tn.webdistribue;
+package com.example.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebDistribueApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
